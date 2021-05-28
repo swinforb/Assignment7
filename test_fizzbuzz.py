3d5ef7bc9b2f0dd9823e171c4809a1b3402cbcd7
swinforb <swinforb@oregonstate.edu>
2021-05-28 13:00:04 -0700

    Created Initial FizzBuzz Test

---
+++ test_fizzbuzz.py
@@ -0,0 +1,19 @@
+###############################
+# Author: Ben Swinford
+# Class: CS 362
+# Assignment: #7
+# Program: test_fizzbuzz.py
+###############################
+
+import unittest
+#import avg
+
+
+class testFizzBuzz(unittest.TestCase):
+
+
+	def test_size(self):
+		size = fizz.fizzBuzz(x)
+		self.assertEqual(size == 100)
+
+
