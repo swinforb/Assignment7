3e24c54af49c807c7af9020ab0d740d9e206b308
swinforb <swinforb@oregonstate.edu>
2021-05-28 14:00:56 -0700

    Added test for correctness of program based of expected answers

--- test_leapyear.py
+++ test_leapyear.py
@@ -15,6 +15,14 @@ class testLeapYear(unittest.TestCase):
 		print("There SHOULD be an failure if the type is correct, not an error")
 		with self.assertRaises(ValueError): leapyear.main()
 
+	def test_correctness(self):
+		ans = leapyear.main()
+		expect = input("Is the year you entered a leap year? (y/n): ")
+		if expect == "y":
+			exAns = 1
+		elif expect == "n":
+			exAns = 0
+		self.assertEqual(exAns, ans)
 
 if __name__ == '__main__':
 	unittest.main()
